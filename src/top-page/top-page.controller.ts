198b2178e3e0516e342d9ac571202be08740cce2
Evgenii Dianov <dianov.js@gmail.com>
2021-09-21 14:10:01 +0300

    Сконфигурировал Mongoose

--- src/top-page/top-page.controller.ts
+++ src/top-page/top-page.controller.ts
@@ -1,37 +1,31 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  HttpCode,
-  Param,
-  Patch,
-  Post,
-} from '@nestjs/common';
-import { ProductModel } from '../product/product.model';
-import { FindProductDto } from '../product/dto/find-product.dto';
-import { TopPageModel } from './top-page.model';
-import { FindTopPageDto } from './dto/find-top-page.dto';
-import { ConfigService } from '@nestjs/config';
+import {Body, Controller, Delete, Get, HttpCode, Param, Patch, Post} from '@nestjs/common';
+import {ProductModel} from "../product/product.model";
+import {FindProductDto} from "../product/dto/find-product.dto";
+import {TopPageModel} from "./top-page.model";
+import {FindTopPageDto} from "./dto/find-top-page.dto";
 
 @Controller('top-page')
 export class TopPageController {
-  constructor(private readonly configService: ConfigService) {}
-  @Post('create')
-  async create(@Body() dto: Omit<TopPageModel, '_id'>) {
-    this.configService.get('THIS');
-  }
 
-  @Get(':id')
-  async get(@Param('id') id: string) {}
+    @Post('create')
+    async create(@Body() dto: Omit<TopPageModel, '_id'>) {
+    }
 
-  @Delete(':id')
-  async delete(@Param('id') id: string) {}
+    @Get(':id')
+    async get(@Param('id') id: string) {
+    }
 
-  @Patch(':id')
-  async patch(@Param('id') id: string, @Body() dto: TopPageModel) {}
+    @Delete(':id')
+    async delete(@Param('id') id: string) {
+    }
 
-  @HttpCode(200)
-  @Post()
-  async find(@Body() dto: FindTopPageDto) {}
+    @Patch(':id')
+    async patch(@Param('id') id: string, @Body() dto:TopPageModel) {
+    }
+
+    @HttpCode(200)
+    @Post()
+    async find(@Body() dto: FindTopPageDto) {
+
+    }
 }
